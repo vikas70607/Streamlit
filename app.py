a4250a3008cb7a3ab5c1caecb5a88bc8cf3641d1
vikas <vikas70607@gmail.com>
2023-07-29 16:53:21 +0530

    First app

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ st.write('''
          # This is a streamlit app
          using yfinance as library
          ''')
+
+tickerSymbol = 'GOOGL'
+
+tickerData = yf.Ticker(tickerSymbol)
+
+tickerDf = tickerData.history(period='id',start='2010-5-31',end='2020-5-31')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
